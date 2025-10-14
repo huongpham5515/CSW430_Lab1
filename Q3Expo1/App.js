5a69081a97d0deffc570db5c96c956533a403943
huongpham5515 <phamthuhuong892003@gmail.com>
2025-10-14 16:09:27 +0700

    InitialCreate

---
+++ Q3Expo1/App.js
@@ -0,0 +1,33 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+import React, { useState } from "react";
+import { Text, Button, View} from 'react-native';
+
+export default () => {
+  const [pressCount, setPressCount] = useState(0);
+
+  return (
+    <View style={{ alignItems: "center", marginTop: 50}}>
+      <Text>Huong has pressed the button: {pressCount} time(s)</Text>
+      <Button title="Press me" onPress={() => setPressCount(pressCount + 1)}/>
+    </View>
+  );
+};
